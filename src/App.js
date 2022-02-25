96e43e7b80bb743894233b75366b87d9a9480ebe
Hatem Lamine <hatelm902@gmail.com>
2022-02-25 15:15:31 +0100

    Map

--- src/App.js
+++ src/App.js
@@ -20,41 +20,70 @@ import { ChevronDownIcon } from '@chakra-ui/icons';
 import './App.css';
 import ColorModeSwitch from './Components/ColorSwitchMode';
 import { useEffect, useState } from 'react';
+import { sortData } from './utils/sort';
+import Map from './Components/Map';
+import 'leaflet/dist/leaflet.css';
+import LineGraph from './Components/LineGraph';
 
 function App() {
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
   const bg = useColorModeValue('whiteAlpha.500', 'whiteAlpha.200');
   const all = 'Worldwide';
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState([]);
+  const [casesType, setCasesType] = useState('cases');
+  const tableData = sortData(
+    countries.filter((country) => country.name != all)
+  );
   const getCountries = async () => {
     await fetch('https://disease.sh/v3/covid-19/countries')
       .then((response) => response.json())
-      .then((data) =>
-        setCountries([{ name: all }, ...data.map((country) => ({ name: country.country, cases: country.cases }))])
-      );
+      .then((data) => {
+        setCountries([
+          { name: all },
+          ...data.map((country) => ({
+            name: country.country,
+            cases: country.cases,
+          })),
+        ]);
+        setMapCountries(data);
+      });
   };
   useEffect(getCountries, []);
   const getCountry = async () => {
     await fetch('https://disease.sh/v3/covid-19/all')
       .then((response) => response.json())
-      .then((data) => setCountry({ name: all, cases: data.cases, recovered: data.recovered, deaths: data.deaths }));
+      .then((data) =>
+        setCountry({
+          name: all,
+          cases: data.cases,
+          recovered: data.recovered,
+          deaths: data.deaths,
+        })
+      );
   };
   useEffect(getCountry, []);
   const onCountryClick = async (e) => {
     const clickedCountry = e.target.value;
     const allParam = clickedCountry === all;
     await fetch(
-      allParam ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${clickedCountry}`
+      allParam
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/covid-19/countries/${clickedCountry}`
     )
       .then((response) => response.json())
-      .then((data) =>
+      .then((data) => {
         setCountry({
           name: allParam ? all : data.country,
           cases: data.cases,
           recovered: data.recovered,
           deaths: data.deaths,
-        })
-      );
+        });
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
+      });
   };
 
   return (
@@ -77,7 +106,11 @@ function App() {
             </MenuButton>
             <MenuList overflow='scroll' height='400px'>
               {countries.map((country) => (
-                <MenuItem key={country.name} value={country.name} onClick={onCountryClick}>
+                <MenuItem
+                  key={country.name}
+                  value={country.name}
+                  onClick={onCountryClick}
+                >
                   {country.name}
                 </MenuItem>
               ))}
@@ -87,14 +120,20 @@ function App() {
 
           <Flex />
           <Flex>
-            <Button variant='ghost'>{country.cases}</Button>
-            <Button variant='ghost'>{country.recovered}</Button>
-            <Button variant='ghost'>{country.deaths}</Button>
+            <Button variant='ghost' onClick={(e) => setCasesType('cases')}>
+              {country.cases}
+            </Button>
+            <Button variant='ghost' onClick={(e) => setCasesType('recovered')}>
+              {country.recovered}
+            </Button>
+            <Button variant='ghost' onClick={(e) => setCasesType('deaths')}>
+              {country.deaths}
+            </Button>
           </Flex>
 
-          <Flex borderRadius='lg' mb={6} p={3} textAlign='center' bg={bg}>
-            GEO MAP
-          </Flex>
+          {/* <Flex borderRadius='lg' mb={6} p={3} textAlign='center' bg={bg}> */}
+
+          {/* </Flex> */}
         </Flex>
         <Flex>
           <Flex>
@@ -107,22 +146,13 @@ function App() {
                   <Th isNumeric>Live Cases</Th>
                 </Tr>
               </Thead>
-              <Tbody>
-                <Tr>
-                  <Td>inches</Td>
-                  <Td>millimeters (mm)</Td>
-                  <Td isNumeric>25.4</Td>
-                </Tr>
-                <Tr>
-                  <Td>feet</Td>
-                  <Td>centimeters (cm)</Td>
-                  <Td isNumeric>30.48</Td>
-                </Tr>
-                <Tr>
-                  <Td>yards</Td>
-                  <Td>metres (m)</Td>
-                  <Td isNumeric>0.91444</Td>
-                </Tr>
+              <Tbody /* overflow='scroll' height='400px' */>
+                {tableData.map((country) => (
+                  <Tr key={country.name}>
+                    <Td>{country.name}</Td>
+                    <Td>{country.cases}</Td>
+                  </Tr>
+                ))}
               </Tbody>
             </Table>
           </Flex>
@@ -134,6 +164,13 @@ function App() {
           </Flex>
         </Flex>
       </Flex>
+      <Map
+        countries={mapCountries}
+        casesType={casesType}
+        center={mapCenter}
+        zoom={mapZoom}
+      />
+      {/* <LineGraph casesType={casesType} /> */}
     </>
   );
 }
